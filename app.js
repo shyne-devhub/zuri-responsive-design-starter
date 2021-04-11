562e534bc96a23a5b7cba17babf8e17c434d19b7
Oloruntoba Oluwaseyi Gabriel <toba25seyi@gmail.com>
2021-04-11 15:35:27 +0100

    functional navigation bar with javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const icon = document.querySelector('.icon');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorALL('.nav-links li');
+
+
+icon.addEventListener('click',()=>{
+  
+})
